2e36752030344f0bbf666a0f5fb54e7361b982ef
venkatakrishnan434 <venkatakrishnan@434@gmail.com>
2022-09-19 16:24:48 +0530

    added new files

---
+++ dynamic.rb
@@ -0,0 +1,28 @@
+def dynamicArray(n, q)
+   
+    arrys = Array.new(n.to_i){[]}
+
+
+lastAnswer = 0
+
+
+q.length.times do
+
+
+     arry = ((x^ lastAns) % n)
+
+    
+    arr = arrys[arry]
+   
+    arr << y if q_type == "1"
+
+        if q_type == "2"
+       
+        idx = y % arr.length
+       
+        lastAnswer = arr[idx]
+        
+        puts lastAnswer
+    end
+end
+end
